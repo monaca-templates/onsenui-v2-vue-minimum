4fd4e7de7afcaee9a596142a5de7a2fd77006981
Adam Kozuch <adam@cmatsudas-MacBook-Air.local>
2017-03-01 12:39:32 +0900

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template id="main">
+    <v-ons-page>
+      <v-ons-toolbar>
+        <div class="center">Onsen UI</div>
+      </v-ons-toolbar>
+      <div>Hello World!</div>
+      <p style="text-align: center">
+        <v-ons-button @click="alert">Click Me!</v-ons-button>
+      </p>
+    </v-ons-page>
+</template>
+<script>
+    export default{
+        methods: {
+          alert() {
+            this.$notification.alert('This is an Onsen UI alert notification test.');
+          }
+        }
+    };
+</script>
