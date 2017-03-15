3b5c33a6f497fdacfe37e2a7c54902a3644ca88f
adamkozuch <adam.kozuch@gmail.com>
2017-03-15 14:46:12 +0900

    fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-ons-page>
       <v-ons-toolbar>
-        <div class="center">Onsen UI</div>
+        <div class="center">{{ title }}</div>
         <div class="right">
           <v-ons-toolbar-button>
             <v-ons-icon icon="ion-navicon, material: md-menu"></v-ons-icon>
@@ -15,11 +15,16 @@
     </v-ons-page>
 </template>
 <script>
-    export default{
-        methods: {
-          alert() {
-            this.$ons.notification.alert('This is an Onsen UI alert notification test.');
-          }
-        }
-    };
+  export default{
+    data() {
+      return {
+        title: 'My app'
+      };
+    },
+    methods: {
+      alert() {
+        this.$ons.notification.alert('This is an Onsen UI alert notification test.');
+      }
+    }
+  };
 </script>
